57952b537d85fb7a0bc88abee03caf1888210dba
marco <marco.mamei@unimore.it>
2017-09-16 18:10:08 +0200

    max pois in map

--- FrontEnd3/www/js/main/map.js
+++ FrontEnd3/www/js/main/map.js
@@ -1,6 +1,13 @@
 var mymap;
 
+var pois;
+var markers = new L.LayerGroup();
+var mIcons;
 
+var minLat = 1000;
+var minLon = 1000;
+var maxLat = -1000;
+var maxLon = -1000;
 
 var dragged = false;
 var centerMarker = null;
@@ -15,7 +22,6 @@ function localize(position) {
         prevLat = position.coords.latitude;
         prevLon = position.coords.longitude;
     }
-
 
 
     if(!dragged)
@@ -33,7 +39,7 @@ function localize(position) {
 
 
     centerMarker.setLatLng([position.coords.latitude, position.coords.longitude]);
-
+    selectMarkers(pois,mymap.getBounds());
 
     window.setTimeout(function () {
         navigator.geolocation.getCurrentPosition(localize)
@@ -41,75 +47,88 @@ function localize(position) {
 }
 
 
-function selectMarkers(pois) {
-    markers = {};
+function selectMarkers(pois,bbox) {
+
+    markers.clearLayers();
+    markers = new L.LayerGroup();
+
+    var visiblePois = [];
+
     for(var type in pois) {
-        console.log(type);
-        if(pois[type])
+        //console.log(type);
+        if (pois[type])
             for (var i = 0; i < pois[type].length; i++) {
+
                 var x = pois[type][i];
                 var lat = x.geometry.coordinates[1];
                 var lon = x.geometry.coordinates[0];
+                if (bbox) {
+                    var ll = bbox.getSouthWest();
+                    var tr = bbox.getNorthEast();
+                    if (ll.lat < lat && lat < tr.lat &&
+                        ll.lng < lon && lon < tr.lng)
+                        visiblePois.push(x)
+                }
+                else visiblePois.push(x)
+            }
+    }
 
-                var info = "<span class='popcontent'>"+format_name(x.display_name)+"<br></span>" +
+    visiblePois.sort(function(a,b) {
+        if(a.importance > b.importance) return 1;
+        if(a.importance < b.importance) return -1;
+        return 0
+    });
+
+
+
+    for(var i=0; i<Math.min(MAX_POIS_IN_MAP,visiblePois.length);i++) {
+        var x = visiblePois[i];
+        var type = x.category;
+        var lat = x.geometry.coordinates[1];
+        var lon = x.geometry.coordinates[0];
+
+        var info = "<span class='popcontent'>"+format_name(x.display_name)+"<br></span>" +
                     "<a  target=\"_top\" href=\"visit.html?type="+type+"&num="+i+"\">Visit</a><br>" +
                     "<span style='color:lightsteelblue'>"+format_name_from(x.display_name)+":"+x.place_id+"</span><br>"+
                     "<span onclick='$(\"#popup\").hide()' class='ui-corner-all ui-icon-delete ui-btn-icon-right ui-btn-active ui-state-persist'></span>";
 
 
 
-                var icon = markerIcons[type];
-                if(!icon) icon = markerIcons["attractions"];
+        var icon = markerIcons[type];
+        if(!icon) icon = markerIcons["attractions"];
 
 
-                var marker = L.marker([lat, lon], {icon: icon, mypopup:info});
+        var marker = L.marker([lat, lon], {icon: icon, mypopup:info});
                 //marker.bindPopup("<h2>"+format_name(x.display_name)+"</h2><br><span style='color:lightsteelblue'>"+format_name_from(x.display_name)+":"+x.place_id+"</span><br><a  target=\"_top\" href=\"visit.html?type="+type+"&num="+i+"\">Visit</a>").openPopup().addTo(markers[type]);
 
-                marker.on('click',function(e) {
+        marker.on('click',function(e) {
                     //console.log(e.target.options.mypopup)
                     $("#popup").html(e.target.options.mypopup);
                     $("#popup").show()
                 });
 
-                if(!markers[type]) markers[type]  = new L.LayerGroup();
-                marker.addTo(markers[type]);
+        marker.addTo(markers);
 
-                minLat = Math.min(minLat, lat);
-                minLon = Math.min(minLon, lon);
-                maxLat = Math.max(maxLat, lat);
-                maxLon = Math.max(maxLon, lon)
-            }
+        minLat = Math.min(minLat, lat);
+        minLon = Math.min(minLon, lon);
+        maxLat = Math.max(maxLat, lat);
+        maxLon = Math.max(maxLon, lon)
     }
+
+    mymap.addLayer(markers)
 }
 
-/*
-TO DO:
-- separa il buonding box da metodo di sopra
-- prova a caricare solo un sample dei punti
-- generalizza la funzione in modo che possa ancare beene anche in next_step senza codice duplicato
-- stessa cosa anche per la funzione che calcola in bbox
-- usa il peso dei pois
- */
 
-var markers = {};//new Markers();
-
-var minLat = 1000;
-var minLon = 1000;
-var maxLat = -1000;
-var maxLon = -1000;
 
 $(document).ready(function(){
+
+
+    mIcons = JSON.parse(window.sessionStorage.getItem("mIcons"));
+
     $("#popup").hide();
-    var pois = JSON.parse(window.sessionStorage.getItem("pois"));
-    //console.log(pois);
-    selectMarkers(pois);
-    //console.log(markers)
+    pois = JSON.parse(window.sessionStorage.getItem("pois"));
+    console.log(pois);
 
-
-
-    var layers = [];
-    for(k in markers)
-        layers.push(markers[k])
 
     mymap = L.map('mapid',{
         attributionControl: false,
@@ -117,23 +136,14 @@ $(document).ready(function(){
         doubleClickZoom: true,
         zoomControl: true,
         touchZoom: true,
-        dragging: true,
-        layers: layers
+        dragging: true
     });
-
-
-    var mIcons = JSON.parse(window.sessionStorage.getItem("mIcons"));
-    console.log(mIcons);
-    for(k in markers) {
-        console.log("*** "+k);
-        var x = mIcons[k].split(",");
-        //console.log(k+"=>"+x)
-        markers["<span style='font-size:30px;color:white;background-color:"+x[2]+"'>&nbsp;<i style='font-size:20px;'class='fa "+x[1]+"'></i>&nbsp;</span> "+k] = markers[k];
-            delete markers[k]
-    }
-    $.when(translateObjKeys(markers)).done(function(){
-        L.control.layers(null,markers).addTo(mymap)}
-    );
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+    selectMarkers(pois);
+    mymap.fitBounds([
+        [minLat, minLon],
+        [maxLat, maxLon]
+    ]);
 
 
     L.easyButton('fa-crosshairs fa-lg', function(btn, map) {
@@ -144,15 +154,16 @@ $(document).ready(function(){
 
     mymap.on('drag', function(e) {
         dragged = true;
+        selectMarkers(pois,mymap.getBounds())
+    });
+
+    mymap.on('zoom', function(e) {
+        selectMarkers(pois,mymap.getBounds())
     });
 
 
-    mymap.fitBounds([
-        [minLat, minLon],
-        [maxLat, maxLon]
-    ]);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+
 
 
     if(conf.localize)
